491202b3308a7b838cdacad498c84df85f292fc8
aarush-27 <aarushsaharan27@gmail.com>
2026-03-23 15:13:28 +0530

    Add files via upload

---
+++ chapter-2/Problems/2-2/2-2-3/2-2-3(b).c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define STACKSIZE 100
+struct stack{
+    int top;
+    int items[STACKSIZE];
+};
+bool empty(struct stack *ps){
+    return ps->top==-1;
+}
+int pop(struct stack *ps){
+    if(empty(ps))
+    {
+        printf("STACK UNDERFLOW");
+        exit(1);
+    }
+    return ps->items[ps->top--];
+}
+void push (struct stack *ps, int x){
+    if(ps->top==STACKSIZE-1)
+    {
+        printf("STACK OVERFLOW");
+        exit(1);
+    }
+    ps->items[++(ps->top)] = x;
+}
+int stacktop(struct stack *ps){
+    if (empty(ps))
+    {
+        printf("STACK UNDERFLOW");
+        exit(1);
+    }
+    return ps->items[ps->top];
+}
+int main()
+{
+    int c;
+    while ((c=getchar())!=EOF)
+    {
+        struct stack s;
+        s.top=-1;
+        while (1)
+        {
+            if (c=='C')
+            {
+                break;
+            }
+            push(&s,c);
+            c=getchar();
+        }
+        while ((c=getchar())!='D' && c!=EOF)
+        {
+            int temp = stacktop(&s);
+            if (temp==c)
+            {
+                pop(&s);
+            }
+            else
+            {
+                printf("NOT COMPATIBLE");
+                exit(1);
+            }
+        }
+        if (!empty(&s))
+        {
+            printf("NOT COMPATIBLE");
+            exit(1);
+        }
+    }
+    printf("COMPATIBLE");
+}
