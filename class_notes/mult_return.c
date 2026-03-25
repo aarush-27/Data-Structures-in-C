bbb79f4e205ad7541edf1d553e9e7149363ac672
aarush-27 <aarushsaharan27@gmail.com>
2026-03-25 13:20:46 +0530

    Add files via upload

---
+++ class_notes/mult_return.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 1000
+
+int getint(int *pn)
+{
+    int c, sign;
+    while (isspace(c=getchar()));
+    if (!isdigit(c)&&c!=EOF&&c!='+'&&c!='-')
+    { 
+        ungetc(c,stdin);
+        return 0;
+    }
+    sign  = (c=='-')? -1 : 1;
+    if (c=='+'|| c=='-')
+    {
+        c = getchar();
+    }
+    for (*pn=0;isdigit(c);c = getchar()) *pn = 10*(*pn)+(c-'0');
+    *pn *=sign;
+    if (c!=EOF) ungetc(c,stdin);
+    return c;
+}
+int maint()
+{
+    int array[SIZE],n,getint(int* n),r;
+    for (n=0;n<SIZE&&getint(&array[n])!=EOF;n++);
+}
